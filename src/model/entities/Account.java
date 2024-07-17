4f691972719524393f5fcf06d4cd90d9fdc97c33
Guilherme Dores <guilhermezdores@gmail.com>
2024-07-17 13:37:03 -0300

    Criado sistema de saque da conta

---
+++ src/model/entities/Account.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	//constructors
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	//methods
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		if (amount > getWithdrawLimit()) {
+			throw new DomainException("Withdraw error: The amount exceds withdraw limit");
+		}
+		if (amount > getBalance()) {
+			throw new DomainException("Withdraw error: Not enough balance");
+		}
+		else {
+			balance -= amount;
+		}
+	}
+	
+	//getter setters
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+}
